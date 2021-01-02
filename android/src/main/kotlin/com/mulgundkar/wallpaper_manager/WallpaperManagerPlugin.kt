069239c8c81d1fb3201afc63fb7fbe1940d3df91
Dagem Amare <dagemamare@gmail.com>
2021-01-03 00:53:54 +0300

    fixed error while compiling

--- android/src/main/kotlin/com/mulgundkar/wallpaper_manager/WallpaperManagerPlugin.kt
+++ android/src/main/kotlin/com/mulgundkar/wallpaper_manager/WallpaperManagerPlugin.kt
@@ -7,7 +7,6 @@ import android.content.Context
 import android.graphics.BitmapFactory
 import android.graphics.Rect
 import android.os.Build
-import androidx.annotation.NonNull
 import io.flutter.embedding.engine.loader.FlutterLoader
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
@@ -31,13 +30,13 @@ class WallpaperManagerPlugin : FlutterPlugin, MethodCallHandler {
     private lateinit var channel: MethodChannel
     private var context: Context? = null
 
-    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
         context = flutterPluginBinding.applicationContext
         channel = MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "wallpaper_manager")
         channel.setMethodCallHandler(this);
     }
 
-    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+    override fun onMethodCall(call: MethodCall, result: Result) {
         when (call.method) {
             "getPlatformVersion" -> result.success("Android ${android.os.Build.VERSION.RELEASE}")
             "setWallpaperFromFile" -> {
@@ -84,7 +83,7 @@ class WallpaperManagerPlugin : FlutterPlugin, MethodCallHandler {
         }
     }
 
-    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
         channel.setMethodCallHandler(null)
         context = null;
     }
